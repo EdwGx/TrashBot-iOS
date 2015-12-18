2e0c471191a32bac6b1784d9942b4cf4410d1d4b
Pei Liang Guo <edwardplguo@gmail.com>
2015-12-18 15:41:33 -0500

    Generate swift xcode project

---
+++ TrashBot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrashBot
+//
+//  Created by Edward Guo on 2015-12-18.
+//  Copyright © 2015 Peiliang Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
