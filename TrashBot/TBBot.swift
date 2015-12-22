7efaaff9b582ce0da84fb92ee136738a203af765
Peiliang Guo <edwardplguo@gmail.com>
2015-12-21 19:34:47 -0500

    Add TBBot class

---
+++ TrashBot/TBBot.swift
@@ -0,0 +1,29 @@
+//
+//  TBBot.swift
+//  TrashBot
+//
+//  Created by Edward Guo on 2015-12-21.
+//  Copyright © 2015 Peiliang Guo. All rights reserved.
+//
+
+import Foundation
+
+enum TBMotionDirection {
+    case Stop
+    case Forward
+    case Backward
+    case TurnLeft
+    case TurnRight
+}
+
+class TBBot : NSObject {
+    var direction = TBMotionDirection.Stop
+    
+    override init() {
+        super.init()
+    }
+    
+    func display(string string: String){
+        NSLog(string)
+    }
+}
